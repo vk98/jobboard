3a4475ba88f556294164e777177b0f5fc59a040c
Viktor Kolev <vikmkltd@gmail.com>
2018-02-22 17:50:57 +0200

    filter

--- filter.js
+++ filter.js
@@ -1,7 +1,24 @@
+var yaml = require('js-yaml');
+var fs = require('fs');
 
 
-function toLower(v) {
-    return v.toLowerCase();
-  }
+function find(string,issue){
+    if (issue.toLowerCase().indexOf("string") != -1) return true;
+    else return false; 
+}
+function match(issue){
+    var doc = yaml.safeLoad(fs.readFileSync('criteria.yaml', 'utf8'));
+    var is_matching=true;
+    doc.filter.forEach(function(name){
+        if(!find(name,issue))is_matching=false;
+    });
 
-function filter()
+    console.log(doc);
+    return is_matching;
+}
+function filter(offers){
+    offers.forEach(function(issue,index, object){
+        if(!match(issue))object.splice(index,1);        
+    });
+    return offers;
+}
