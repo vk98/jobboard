18b1624bf691fa57581925004717e7539e0afb63
Ioan Stoianov <jstoyanov@obecto.com>
2018-02-22 17:44:31 +0200

    Printing

--- offers.js
+++ offers.js
@@ -24,25 +24,20 @@ function getAllOffers() {
   });
 }
 
-// var Table = require('cli-table');
-// // instantiate
-// var table = new Table({
-//     head: ['TH 1 label', 'TH 2 label']
-//   , colWidths: [20, 40]
-// });
-// var data = getAllOffers().then((data)=>{
-//   for(let da of data){
-//     console.log(da.number)
-//     console.log(da.title)
-//     table.push(
-//       [da.number, da.title]
-//   );
-//   }
-// });
-// // table is an Array, so you can `push`, `unshift`, `splice` and friends
-// table.push(
-//     ['First value', 'Second value']
-//   , ['First value', 'Second value']
-// );
-
-// console.log(table.toString());
+var Table = require('cli-table');
+// instantiate
+var table = new Table({
+    head: ['TH 1 label', 'TH 2 label']
+  , colWidths: [20, 40]
+});
+var data = getAllOffers().then((data)=>{
+  for(let da of data){
+    console.log(da.number)
+    console.log(da.title)
+    table.push(
+      [da.number, da.title]
+  );
+  console.log(table.toString());
+  }
+});
+// table is an Array, so you can `push`, `unshift`, `splice` and friends
